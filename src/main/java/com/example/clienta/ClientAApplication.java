ef53b7a99fe9a9e3a379901342e3a6ed2d4b4a2b
Joan Marc Riera Duocastella <joanmarcriera@gmail.com>
2018-08-06 23:14:09 +0100

    Web rest client

---
+++ src/main/java/com/example/clienta/ClientAApplication.java
@@ -0,0 +1,21 @@
+package com.example.clienta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@RestController
+public class ClientAApplication {
+
+	@GetMapping ("/")
+	public String name(){
+		return "Client-a";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(ClientAApplication.class, args);
+	}
+}
